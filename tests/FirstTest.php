4e1efcedcd04430235a2616dd335628ee8f03be5
Ilya Troy <troytft@gmail.com>
2019-09-29 01:32:38 +0300

    add phpunit, phpcs, phpstan, travis ci

---
+++ tests/FirstTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase
+{
+    public function testPhpunitWorks()
+    {
+        $this->assertTrue(true);
+    }
+}
