aae05789f16bba86da680a9308bf6baafc9f1c07
Ilya Troy <troytft@gmail.com>
2019-10-06 00:53:54 +0200

    handle error

--- src/Manager/RequestModelManager.php
+++ src/Manager/RequestModelManager.php
@@ -26,7 +26,31 @@ class RequestModelManager
             $this->mapper->map($requestModel, $data);
         } catch (Mapper\Exception\ExceptionInterface $exception) {
             if ($exception instanceof Mapper\Exception\MappingValidation\CollectionRequiredException) {
-                throw new RequestModelMappingException([$exception->getPathAsString() => ['Value should be a collection']]);
+                throw new RequestModelMappingException([$exception->getPathAsString() => ['This value should be collection.']]);
+            } elseif ($exception instanceof Mapper\Exception\MappingValidation\ObjectRequiredException) {
+                throw new RequestModelMappingException([$exception->getPathAsString() => ['This value should be object.']]);
+            } elseif ($exception instanceof Mapper\Exception\MappingValidation\ScalarRequiredException) {
+                throw new RequestModelMappingException([$exception->getPathAsString() => ['This value should be scalar.']]);
+            } elseif ($exception instanceof Mapper\Exception\MappingValidation\UndefinedKeyException) {
+                throw new RequestModelMappingException([$exception->getPathAsString() => ['Key is not defined in model.']]);
+            } elseif ($exception instanceof Mapper\Exception\Transformer\WrappedTransformerException) {
+                $previousException = $exception->getPrevious();
+
+                if ($previousException instanceof Mapper\Exception\Transformer\BooleanRequiredException) {
+                    throw new RequestModelMappingException([$exception->getPathAsString() => ['This value should be boolean.']]);
+                } elseif ($previousException instanceof Mapper\Exception\Transformer\FloatRequiredException) {
+                    throw new RequestModelMappingException([$exception->getPathAsString() => ['This value should be float.']]);
+                } elseif ($exception instanceof Mapper\Exception\Transformer\IntegerRequiredException) {
+                    throw new RequestModelMappingException([$exception->getPathAsString() => ['This value should be integer.']]);
+                } elseif ($exception instanceof Mapper\Exception\Transformer\InvalidDateFormatException) {
+                    throw new RequestModelMappingException([$exception->getPathAsString() => [sprintf('This value should be valid date with format "%s".', $exception->getFormat())]]);
+                } elseif ($exception instanceof Mapper\Exception\Transformer\InvalidDateTimeFormatException) {
+                    throw new RequestModelMappingException([$exception->getPathAsString() => [sprintf('This value should be valid date time with format "%s".', $exception->getFormat())]]);
+                } elseif ($exception instanceof Mapper\Exception\Transformer\IntegerRequiredException) {
+                    throw new RequestModelMappingException([$exception->getPathAsString() => ['This value should be integer.']]);
+                } else {
+                    throw new \InvalidArgumentException();
+                }
             } else {
                 throw new \RuntimeException(sprintf('Unhandled exception %s %s', get_class($exception), $exception->getMessage()));
             }
