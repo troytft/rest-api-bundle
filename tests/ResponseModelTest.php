1e4e79f82b059a5e6f848d3235480ecfcf81ae2c
Ilya Troy <troytft@gmail.com>
2020-01-14 12:16:06 +0100

    add response tests

---
+++ tests/ResponseModelTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests;
+
+use Symfony\Component\HttpFoundation\Request;
+use Tests;
+
+class ResponseModelTest extends BaseBundleTestCase
+{
+    public function testSerializer()
+    {
+        $entity = new Tests\DemoApp\DemoBundle\Entity\Genre();
+        $entity
+            ->setId(13)
+            ->setSlug('genre-slug');
+
+        $json = $this->getResponseModelSerializer()->toJson(new Tests\DemoApp\DemoBundle\ResponseModel\Genre($entity));
+
+        $this->assertSame('{"id":13,"slug":"genre-slug","__typename":"Genre"}', $json);
+    }
+
+    public function testResponseWithNull()
+    {
+        $request = Request::create('http://localhost/demo-responses/null', 'GET');
+        $response = $this->getKernel()->handle($request);
+
+        $this->assertSame(204, $response->getStatusCode());
+        $this->assertSame('', $response->getContent());
+    }
+
+    public function testResponseWithSingleResponseModel()
+    {
+        $request = Request::create('http://localhost/demo-responses/single-response-model', 'GET');
+        $response = $this->getKernel()->handle($request);
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertSame('{"id":1,"slug":"demo-slug","__typename":"Genre"}', $response->getContent());
+    }
+
+    public function testResponseWithCollectionOfResponseModels()
+    {
+        $request = Request::create('http://localhost/demo-responses/collection-of-response-models', 'GET');
+        $response = $this->getKernel()->handle($request);
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertSame('[{"id":1,"slug":"1-demo-slug","__typename":"Genre"},{"id":2,"slug":"2-demo-slug","__typename":"Genre"},{"id":3,"slug":"3-demo-slug","__typename":"Genre"}]', $response->getContent());
+    }
+
+    public function testResponseWithResponseClass()
+    {
+        $request = Request::create('http://localhost/demo-responses/response-class', 'GET');
+        $response = $this->getKernel()->handle($request);
+
+        $this->assertSame(201, $response->getStatusCode());
+        $this->assertSame('{"id": 7}', $response->getContent());
+    }
+}
