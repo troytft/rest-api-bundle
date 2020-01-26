b370b65e6855fdbe75e9135563cdaaab4d9964de
Ilya Troy <troytft@gmail.com>
2020-01-26 13:46:46 +0300

    use yaml as example

--- tests/Docs/GenerateDocsCommandTest.php
+++ tests/Docs/GenerateDocsCommandTest.php
@@ -5,7 +5,7 @@ namespace Tests\Docs;
 use Tests;
 use Symfony\Component\Console\Tester\CommandTester;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\Yaml\Yaml;
+use function file_get_contents;
 use function sys_get_temp_dir;
 use function tempnam;
 
@@ -23,70 +23,53 @@ class GenerateDocsCommandTest extends Tests\BaseBundleTestCase
             '--controller-namespace-prefix' => Tests\DemoApp\DemoBundle\Controller\DemoController::class,
         ]);
 
-        $generatedData = Yaml::parseFile($outputFile);
-
         $this->assertSame(0, $commandTester->getStatusCode());
-        $this->assertSame([
-            'openapi' => '3.0.0',
-            'info' => [
-                'title' => 'Open API Specification',
-                'version' => '1.0.0',
-            ],
-            'paths' => [
-                '/genres/by-slug/{genre}' => [
-                    'get' => [
-                        'summary' => 'Genre response model details',
-                        'responses' => [
-                            200 => [
-                                'description' => 'Success response with body',
-                                'content' => [
-                                    'application/json' => [
-                                        'schema' => [
-                                            'type' => 'object',
-                                            'properties' => [
-                                                'id' => [
-                                                    'type' => 'integer',
-                                                    'nullable' => false,
-                                                ],
-                                                'slug' => [
-                                                    'type' => 'string',
-                                                    'nullable' => false,
-                                                ],
-                                                '__typename' => [
-                                                    'type' => 'string',
-                                                    'nullable' => false,
-                                                ],
-                                            ],
-                                            'nullable' => false,
-                                        ],
-                                    ],
-                                ],
-                            ],
-                        ],
-                        'parameters' => [
-                            [
-                                'name' => 'genre',
-                                'in' => 'path',
-                                'description' => 'String regex format is "\\d+".',
-                                'required' => true,
-                                'schema' => [
-                                    'type' => 'string',
-                                    'nullable' => false,
-                                ],
-                            ],
-                        ],
-                        'tags' => [
-                            'demo',
-                        ],
-                    ],
-                ],
-            ],
-            'tags' => [
-                [
-                    'name' => 'demo',
-                ],
-            ],
-        ], $generatedData);
+
+        $expextedYaml = <<<YAML
+openapi: 3.0.0
+info:
+  title: 'Open API Specification'
+  version: 1.0.0
+paths:
+  '/genres/by-slug/{genre}':
+    get:
+      summary: 'Genre response model details'
+      responses:
+        '200':
+          description: 'Success response with body'
+          content:
+            application/json:
+              schema:
+                type: object
+                properties:
+                  id:
+                    type: integer
+                    nullable: false
+                  slug:
+                    type: string
+                    nullable: false
+                  __typename:
+                    type: string
+                    nullable: false
+                nullable: false
+      parameters:
+        -
+          name: genre
+          in: path
+          description: 'String regex format is "\d+".'
+          required: true
+          schema:
+            type: string
+            nullable: false
+      tags:
+        - demo
+tags:
+  -
+    name: demo
+
+YAML;
+
+        $this->assertSame($expextedYaml, file_get_contents($outputFile));
     }
 
     public function testInvalidDefinitionError()
