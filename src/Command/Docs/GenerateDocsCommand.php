cb417f96d672514c515cde9f8745bf5053f965be
Ilya Troy <troytft@gmail.com>
2020-01-26 14:54:50 +0300

    fix file ident

--- src/Command/Docs/GenerateDocsCommand.php
+++ src/Command/Docs/GenerateDocsCommand.php
@@ -1,18 +1,18 @@
 <?php
 
-namespace RestApiBundle\Command;
+namespace RestApiBundle\Command\Docs;
 
 use RestApiBundle;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Style\SymfonyStyle;
 use function sprintf;
 
 class GenerateDocsCommand extends Command
 {
     private const OUTPUT_OPTION = 'output';
+    private const TEMPLATE_OPTION = 'template';
     private const CONTROLLER_NAMESPACE_PREFIX_OPTION = 'controller-namespace-prefix';
 
     protected static $defaultName = 'rest-api:generate-docs';
@@ -27,27 +27,33 @@ class GenerateDocsCommand extends Command
      */
     private $openApiRootSchemaResolver;
 
+    /**
+     * @var RestApiBundle\Services\Docs\OpenApi\SchemaFileWriter
+     */
+    private $schemaFileWriter;
+
     public function __construct(
         RestApiBundle\Services\Docs\RouteDataExtractor $routeDataExtractor,
-        RestApiBundle\Services\Docs\OpenApi\RootSchemaResolver $openApiRootSchemaResolver
+        RestApiBundle\Services\Docs\OpenApi\RootSchemaResolver $openApiRootSchemaResolver,
+        RestApiBundle\Services\Docs\OpenApi\SchemaFileWriter $schemaFileWriter
     ) {
         parent::__construct();
 
         $this->routeDataExtractor = $routeDataExtractor;
         $this->openApiRootSchemaResolver = $openApiRootSchemaResolver;
+        $this->schemaFileWriter = $schemaFileWriter;
     }
 
     protected function configure()
     {
         $this
             ->addOption(static::OUTPUT_OPTION, null, InputOption::VALUE_REQUIRED, 'Path to output file.')
+            ->addOption(static::TEMPLATE_OPTION, null, InputOption::VALUE_REQUIRED, 'Path to template file.')
             ->addOption(static::CONTROLLER_NAMESPACE_PREFIX_OPTION, null, InputOption::VALUE_REQUIRED, 'Prefix for controller namespace.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $io = new SymfonyStyle($input, $output);
-
         if (!$input->getOption(static::OUTPUT_OPTION)) {
             $output
                 ->writeln('Output file option not specified.');
@@ -70,7 +76,7 @@ class GenerateDocsCommand extends Command
 
         $rootSchema = $this->openApiRootSchemaResolver->resolve($routeDataItems);
 
-        \cebe\openapi\Writer::writeToYamlFile($rootSchema, $input->getOption(static::OUTPUT_OPTION));
+        $this->schemaFileWriter->writeToYamlFile($rootSchema, $input->getOption(static::OUTPUT_OPTION));
 
         return 0;
     }
