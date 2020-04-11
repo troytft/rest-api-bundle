31c71db485fdf275bb27c70bdd0e493552fcc84c
Ilya Troy <troytft@gmail.com>
2020-04-12 02:38:54 +0300

    add query parameter with request model

--- src/Services/Docs/OpenApiSpecificationGenerator.php
+++ src/Services/Docs/OpenApiSpecificationGenerator.php
@@ -5,6 +5,7 @@ namespace RestApiBundle\Services\Docs;
 use RestApiBundle;
 use cebe\openapi\spec as OpenApi;
 use Symfony\Component\Yaml\Yaml;
+use function array_merge;
 use function array_values;
 use function json_encode;
 use function strtolower;
@@ -92,32 +93,40 @@ class OpenApiSpecificationGenerator
                 ]));
             }
 
-            $operation = new OpenApi\Operation([
-                'summary' => $routeData->getTitle(),
-                'responses' => $responses,
-            ]);
-
-            $parameters = [];
+            $pathParameters = [];
 
             foreach ($routeData->getPathParameters() as $pathParameter) {
-                $parameters[] = $this->convertPathParameter($pathParameter);
-            }
-
-            if ($parameters) {
-                $operation->parameters = $parameters;
-            }
-
-            if ($routeData->getTags()) {
-                $operation->tags = $routeData->getTags();
-            }
-
-            if ($routeData->getDescription()) {
-                $operation->description = $routeData->getDescription();
+                $pathParameters[] = $this->createParameter('path', $pathParameter->getName(), $pathParameter->getSchema());
             }
 
             $pathItem = new OpenApi\PathItem([]);
+
             foreach ($routeData->getMethods() as $method) {
                 $method = strtolower($method);
+                $isHttpGetMethod = $method === 'get';
+
+                $operation = new OpenApi\Operation([
+                    'summary' => $routeData->getTitle(),
+                    'responses' => $responses,
+                ]);
+
+                if ($routeData->getTags()) {
+                    $operation->tags = $routeData->getTags();
+                }
+
+                if ($routeData->getDescription()) {
+                    $operation->description = $routeData->getDescription();
+                }
+
+                $queryParameters = [];
+                if ($routeData->getRequestModel() && $isHttpGetMethod) {
+                    $queryParameters = $this->convertRequestModelToParameters($routeData->getRequestModel());
+                }
+
+                if ($pathParameters || $queryParameters) {
+                    $operation->parameters = array_merge($pathParameters, $queryParameters);
+                }
+
                 $pathItem->{$method} = $operation;
             }
 
@@ -129,13 +138,35 @@ class OpenApiSpecificationGenerator
         return $root;
     }
 
-    private function convertPathParameter(RestApiBundle\DTO\Docs\PathParameter $pathParameter): OpenApi\Parameter
+    /**
+     * @param RestApiBundle\DTO\Docs\Schema\ObjectType $objectType
+     *
+     * @return OpenApi\Parameter
+     */
+    private function convertRequestModelToParameters(RestApiBundle\DTO\Docs\Schema\ObjectType $objectType): array
+    {
+        $result = [];
+
+        foreach ($objectType->getProperties() as $name => $property) {
+            if ($property instanceof RestApiBundle\DTO\Docs\Schema\ScalarInterface) {
+                $result[] = $this->createParameter('query', $name, $property);
+            } elseif ($property instanceof RestApiBundle\DTO\Docs\Schema\ArrayType && $property->getInnerType() instanceof RestApiBundle\DTO\Docs\Schema\ScalarInterface) {
+                $result[] = $this->createParameter('query', $name, $property->getInnerType());
+            } else {
+                throw new \InvalidArgumentException();
+            }
+        }
+
+        return $result;
+    }
+
+    private function createParameter(string $type, string $name, RestApiBundle\DTO\Docs\Schema\SchemaTypeInterface $schema): OpenApi\Parameter
     {
         return new OpenApi\Parameter([
-            'in' => 'path',
-            'name' => $pathParameter->getName(),
-            'schema' => $this->convertSchemaType($pathParameter->getSchema()),
-            'required' => !$pathParameter->getSchema()->getNullable(),
+            'in' => $type,
+            'name' => $name,
+            'schema' => $this->convertSchemaType($schema),
+            'required' => !$schema->getNullable(),
         ]);
     }
 
@@ -144,7 +175,7 @@ class OpenApiSpecificationGenerator
         if ($schemaType instanceof RestApiBundle\DTO\Docs\Schema\ObjectType) {
             $result = $this->convertObjectType($schemaType);
         } elseif ($schemaType instanceof RestApiBundle\DTO\Docs\Schema\ArrayType) {
-            $result = $this->convertCollectionType($schemaType);
+            $result = $this->convertArrayType($schemaType);
         } elseif ($schemaType instanceof RestApiBundle\DTO\Docs\Schema\StringType) {
             $result = $this->convertStringType($schemaType);
         } elseif ($schemaType instanceof RestApiBundle\DTO\Docs\Schema\IntegerType) {
@@ -175,13 +206,13 @@ class OpenApiSpecificationGenerator
         ]);
     }
 
-    private function convertCollectionType(RestApiBundle\DTO\Docs\Schema\ArrayType $collectionType): OpenApi\Schema
+    private function convertArrayType(RestApiBundle\DTO\Docs\Schema\ArrayType $arrayType): OpenApi\Schema
     {
         return new OpenApi\Schema([
             'type' => OpenApi\Type::ARRAY,
-            'nullable' => $collectionType->getNullable(),
+            'nullable' => $arrayType->getNullable(),
             'items' => [
-                $this->convertSchemaType($collectionType->getInnerType())
+                $this->convertSchemaType($arrayType->getInnerType())
             ]
         ]);
     }
