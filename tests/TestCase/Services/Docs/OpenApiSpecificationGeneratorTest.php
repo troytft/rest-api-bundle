e60663a08a69fcd7c2472f7ead6acbc2f7f51e19
Ilya Troy <troytft@gmail.com>
2020-04-13 23:29:21 +0300

    add convert request model to parameters test

--- tests/TestCase/Services/Docs/OpenApiSpecificationGeneratorTest.php
+++ tests/TestCase/Services/Docs/OpenApiSpecificationGeneratorTest.php
@@ -5,6 +5,8 @@ namespace Tests\TestCase\Services\Docs;
 use RestApiBundle;
 use Tests;
 use cebe\openapi\spec as OpenApi;
+use function var_dump;
+use function var_export;
 
 class OpenApiSpecificationGeneratorTest extends Tests\TestCase\BaseTestCase
 {
@@ -54,5 +56,42 @@ class OpenApiSpecificationGeneratorTest extends Tests\TestCase\BaseTestCase
         ];
 
         $this->assertSame($expected, $this->convertStdClassToArray($openApiSchema->getSerializableData()));
+    }
+
+    public function testConvertRequestModelToParameters()
+    {
+        $objectProperties = [
+            'offset' => new RestApiBundle\DTO\Docs\Schema\IntegerType(false),
+            'limit' => new RestApiBundle\DTO\Docs\Schema\IntegerType(false),
+        ];
+        $objectType = new RestApiBundle\DTO\Docs\Schema\ObjectType($objectProperties, false);
+
+        /** @var OpenApi\Parameter[] $openApiParameters */
+        $openApiParameters = $this->invokePrivateMethod($this->getOpenApiSpecificationGenerator(), 'convertRequestModelToParameters', [$objectType]);
+
+        $this->assertIsArray($openApiParameters);
+        $this->assertCount(2, $openApiParameters);
+
+        $this->assertInstanceOf(OpenApi\Parameter::class, $openApiParameters[0]);
+        $this->assertSame([
+            'name' => 'offset',
+            'in' => 'query',
+            'required' => true,
+            'schema' => [
+                'type' => 'integer',
+                'nullable' => false,
+            ],
+        ], $this->convertStdClassToArray($openApiParameters[0]->getSerializableData()));
+
+        $this->assertInstanceOf(OpenApi\Parameter::class, $openApiParameters[1]);
+        $this->assertSame([
+            'name' => 'limit',
+            'in' => 'query',
+            'required' => true,
+            'schema' => [
+                'type' => 'integer',
+                'nullable' => false,
+            ],
+        ], $this->convertStdClassToArray($openApiParameters[1]->getSerializableData()));
     }
 }
