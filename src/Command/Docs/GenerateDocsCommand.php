4bbc37785dece9d0746a4aec42839a490b3da24f
Ilya Troy <troytft@gmail.com>
2020-04-21 15:14:31 +0300

    Revert "Revert "write to file instead stdout (#71)""
    This reverts commit 4512a2a6cd9b4f6af0778fe97ea53b0e7bc60e36.

--- src/Command/Docs/GenerateDocsCommand.php
+++ src/Command/Docs/GenerateDocsCommand.php
@@ -3,6 +3,7 @@
 namespace RestApiBundle\Command\Docs;
 
 use RestApiBundle;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -12,6 +13,7 @@ use function sprintf;
 
 class GenerateDocsCommand extends Command
 {
+    private const ARGUMENT_OUTPUT = 'output';
     private const OPTION_NAMESPACE_FILTER = 'namespace-filter';
     private const OPTION_FORMAT = 'format';
 
@@ -32,12 +34,14 @@ class GenerateDocsCommand extends Command
     protected function configure()
     {
         $this
+            ->addArgument(static::ARGUMENT_OUTPUT, InputArgument::REQUIRED, 'Path to output file.')
             ->addOption(static::OPTION_NAMESPACE_FILTER, null, InputOption::VALUE_REQUIRED, 'Prefix for controller namespace filter.')
             ->addOption(static::OPTION_FORMAT, null, InputOption::VALUE_REQUIRED, 'File format json or yaml.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $outputFileName = $input->getArgument(static::ARGUMENT_OUTPUT);
         $namespaceFilter = $input->getOption(static::OPTION_NAMESPACE_FILTER);
         $format = $input->getOption(static::OPTION_FORMAT) ?? RestApiBundle\Enum\Docs\Format::YAML;
 
@@ -48,7 +52,7 @@ class GenerateDocsCommand extends Command
         }
 
         try {
-            $specification = $this->docsGenerator->generateSpecification($format, $namespaceFilter);
+            $this->docsGenerator->writeToFile($outputFileName, $format, $namespaceFilter);
         } catch (RestApiBundle\Exception\Docs\InvalidDefinitionException $exception) {
             $output->writeln(sprintf(
                 'Definition error in %s with message "%s"',
@@ -58,8 +62,6 @@ class GenerateDocsCommand extends Command
 
             return 1;
         }
-
-        $output->write($specification);
 
         return 0;
     }
