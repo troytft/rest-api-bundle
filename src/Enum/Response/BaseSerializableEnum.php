f743e703621efcd97d5ba9738bf3aaef852da2aa
Ilya Troy <troytft@gmail.com>
2021-02-28 15:25:53 +0300

    f

--- src/Enum/Response/BaseSerializableEnum.php
+++ src/Enum/Response/BaseSerializableEnum.php
@@ -4,9 +4,6 @@ namespace RestApiBundle\Enum\Response;
 
 use RestApiBundle;
 
-use function is_int;
-use function is_string;
-
 abstract class BaseSerializableEnum implements RestApiBundle\Enum\Response\SerializableEnumInterface
 {
     /**
@@ -19,17 +16,7 @@ abstract class BaseSerializableEnum implements RestApiBundle\Enum\Response\Seria
      */
     private function __construct($value)
     {
-        if (!is_int($value) && !is_string($value)) {
-            throw new \InvalidArgumentException();
-        }
-    }
-
-    /**
-     * @param int|string $value
-     */
-    public static function from($value)
-    {
-        return new static($value);
+        $this->value = $value;
     }
 
     /**
