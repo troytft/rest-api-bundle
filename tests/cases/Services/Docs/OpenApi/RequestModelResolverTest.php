f5280bf9b3a020c95aff4a8226f9cc829150e737
Ilya Troy <troytft@gmail.com>
2021-03-24 15:55:56 +0300

    snapshot testing (#118)
    * snapshot testing
    
    * snapshot testing
    
    * snapshot testing
    
    * f

--- tests/cases/Services/Docs/OpenApi/RequestModelResolverTest.php
+++ tests/cases/Services/Docs/OpenApi/RequestModelResolverTest.php
@@ -4,103 +4,20 @@ class RequestModelResolverTest extends Tests\BaseTestCase
 {
     public function testNestedModelWithConstraints()
     {
-        $expected = <<<JSON
-{
-    "type": "object",
-    "properties": {
-        "stringField": {
-            "type": "string",
-            "nullable": false,
-            "minLength": 6,
-            "maxLength": 255
-        },
-        "modelField": {
-            "type": "object",
-            "properties": {
-                "stringField": {
-                    "type": "string",
-                    "nullable": false,
-                    "minLength": 3,
-                    "maxLength": 255
-                }
-            },
-            "nullable": false
-        },
-        "collectionField": {
-            "type": "array",
-            "items": {
-                "type": "object",
-                "properties": {
-                    "stringField": {
-                        "type": "string",
-                        "nullable": false,
-                        "minLength": 3,
-                        "maxLength": 255
-                    }
-                },
-                "nullable": false
-            },
-            "nullable": false
-        },
-        "collectionOfIntegers": {
-            "type": "array",
-            "items": {
-                "type": "integer",
-                "nullable": false
-            },
-            "nullable": false
-        }
-    },
-    "nullable": false
-}
-JSON;
-
         $schema = $this->getRequestModelResolver()->resolveByClass(TestApp\RequestModel\ModelWithValidation::class);
-        $this->assertJsonStringEqualsJsonString($expected, json_encode($schema->getSerializableData()));
+        $this->assertMatchesJsonSnapshot(json_encode($schema->getSerializableData()));
     }
 
     public function testModelWithEntityType()
     {
-        $expected = <<<JSON
-{
-    "type": "object",
-    "properties": {
-        "book": {
-            "type": "string",
-            "description": "Element by \"slug\"",
-            "nullable": false
-        }
-    },
-    "nullable": false
-}
-JSON;
-
         $schema = $this->getRequestModelResolver()->resolveByClass(TestApp\RequestModel\ModelWithEntityBySlug::class);
-        $this->assertJsonStringEqualsJsonString($expected, json_encode($schema->getSerializableData()));
+        $this->assertMatchesJsonSnapshot(json_encode($schema->getSerializableData()));
     }
 
     public function testModelWithArrayOfEntitiesType()
     {
-        $expected = <<<JSON
-{
-    "type": "object",
-    "properties": {
-        "books": {
-            "type": "array",
-            "items": {
-                "type": "integer",
-                "nullable": false
-            },
-            "description": "Array of elements by \"id\"",
-            "nullable": false
-        }
-    },
-    "nullable": false
-}
-JSON;
-
         $schema = $this->getRequestModelResolver()->resolveByClass(TestApp\RequestModel\ModelWithArrayOfEntities::class);
-        $this->assertJsonStringEqualsJsonString($expected, json_encode($schema->getSerializableData()));
+        $this->assertMatchesJsonSnapshot(json_encode($schema->getSerializableData()));
     }
 
     private function getRequestModelResolver(): RestApiBundle\Services\Docs\OpenApi\RequestModelResolver
