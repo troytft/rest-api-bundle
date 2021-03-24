7869fa0b39f9855cc09bbe02ce93522575c63f46
Ilya Troy <troytft@gmail.com>
2021-03-24 17:50:46 +0300

    fix symfony 4.4 compatibility (#121)

--- src/Services/Response/EnumNormalizer.php
+++ src/Services/Response/EnumNormalizer.php
@@ -7,7 +7,7 @@ use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
 
 class EnumNormalizer implements ContextAwareNormalizerInterface
 {
-    public function supportsNormalization($data, string $format = null, array $context = [])
+    public function supportsNormalization($data, $format = null, array $context = [])
     {
         return $data instanceof RestApiBundle\Enum\Response\SerializableEnumInterface;
     }
@@ -15,7 +15,7 @@ class EnumNormalizer implements ContextAwareNormalizerInterface
     /**
      * @param RestApiBundle\Enum\Response\SerializableEnumInterface $object
      */
-    public function normalize($object, string $format = null, array $context = [])
+    public function normalize($object, $format = null, array $context = [])
     {
         return $object->getValue();
     }
