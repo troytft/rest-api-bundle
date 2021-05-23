f2dc153de54362c9205806809df9aebc1198db76
Ilya Troy <troytft@gmail.com>
2021-05-23 18:41:46 +0300

    f

--- src/Services/OpenApi/AbstractSchemaResolver.php
+++ src/Services/OpenApi/AbstractSchemaResolver.php
@@ -8,49 +8,6 @@ use cebe\openapi\spec as OpenApi;
 
 abstract class AbstractSchemaResolver
 {
-    protected function resolveScalarTypeOld(RestApiBundle\Model\OpenApi\Types\ScalarInterface $scalarType): OpenApi\Schema
-    {
-        switch (true) {
-            case $scalarType instanceof RestApiBundle\Model\OpenApi\Types\StringType:
-                $result = new OpenApi\Schema([
-                    'type' => OpenApi\Type::STRING,
-                    'nullable' => $scalarType->getNullable(),
-                ]);
-
-                break;
-
-            case $scalarType instanceof RestApiBundle\Model\OpenApi\Types\IntegerType:
-                $result = new OpenApi\Schema([
-                    'type' => OpenApi\Type::INTEGER,
-                    'nullable' => $scalarType->getNullable(),
-                ]);
-
-                break;
-
-            case $scalarType instanceof RestApiBundle\Model\OpenApi\Types\FloatType:
-                $result = new OpenApi\Schema([
-                    'type' => OpenApi\Type::NUMBER,
-                    'format' => 'double',
-                    'nullable' => $scalarType->getNullable(),
-                ]);
-
-                break;
-
-            case $scalarType instanceof RestApiBundle\Model\OpenApi\Types\BooleanType:
-                $result = new OpenApi\Schema([
-                    'type' => OpenApi\Type::BOOLEAN,
-                    'nullable' => $scalarType->getNullable(),
-                ]);
-
-                break;
-
-            default:
-                throw new \InvalidArgumentException();
-        }
-
-        return $result;
-    }
-
     protected function resolveScalarType(PropertyInfo\Type $type): OpenApi\Schema
     {
         switch ($type->getBuiltinType()) {
