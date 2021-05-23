17c03c9e5f1f68635943802ce051d7601911921b
Ilya Troy <troytft@gmail.com>
2021-05-23 19:16:52 +0300

    f

--- src/Services/OpenApi/EndpointFinder.php
+++ src/Services/OpenApi/EndpointFinder.php
@@ -16,7 +16,6 @@ use function array_slice;
 use function count;
 use function explode;
 use function implode;
-use function in_array;
 use function is_array;
 use function is_string;
 use function preg_match_all;
