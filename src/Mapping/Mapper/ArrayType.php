184add5fe7226ecf8a8607f0df0767bba27b2146
Ilya Troy <troytft@gmail.com>
2021-07-22 22:46:07 +0300

    fix array type contructor

--- src/Mapping/Mapper/ArrayType.php
+++ src/Mapping/Mapper/ArrayType.php
@@ -9,14 +9,19 @@ use RestApiBundle;
  */
 class ArrayType implements RestApiBundle\Mapping\Mapper\TypeInterface
 {
+    public $type;
     public bool $nullable = false;
 
-    /**
-     * Type hint forced to object, cause annotation reader doesn't support interfaces
-     *
-     * @var object
-     */
-    public $type;
+    public function __construct(array $options = [])
+    {
+        if (isset($options['value'])) {
+            $this->type = $options['value'];
+        } elseif (isset($options['type'])) {
+            $this->type = $options['type'];
+        } else {
+            throw new \LogicException();
+        }
+    }
 
     public function getValuesType(): RestApiBundle\Mapping\Mapper\TypeInterface
     {
