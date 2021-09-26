ad8821e6227ee3f98af0d5b2b4e38732cfc56d6e
Ilya Troy <troytft@gmail.com>
2021-09-27 02:00:05 +0300

    fix entity type detect

--- src/Helper/DoctrineHelper.php
+++ src/Helper/DoctrineHelper.php
@@ -6,6 +6,8 @@ use RestApiBundle;
 use Doctrine;
 use Symfony\Component\PropertyInfo;
 
+use function in_array;
+
 class DoctrineHelper extends RestApiBundle\Services\OpenApi\AbstractSchemaResolver
 {
     public static function isEntity(string $class): bool
@@ -15,24 +17,31 @@ class DoctrineHelper extends RestApiBundle\Services\OpenApi\AbstractSchemaResolv
         return (bool) RestApiBundle\Helper\AnnotationReader::getClassAnnotation($reflectionClass, Doctrine\ORM\Mapping\Entity::class);
     }
 
-    public static function extractColumnType(string $class, string $field): ?string
+    public static function extractColumnType(string $class, string $field): string
     {
         $reflectionClass = RestApiBundle\Helper\ReflectionClassStore::get($class);
         $reflectionProperty = $reflectionClass->getProperty($field);
 
+
         $columnAnnotation = RestApiBundle\Helper\AnnotationReader::getPropertyAnnotation($reflectionProperty, Doctrine\ORM\Mapping\Column::class);
         if (!$columnAnnotation instanceof Doctrine\ORM\Mapping\Column) {
-            return null;
+            throw new \InvalidArgumentException();
         }
 
-        if ($columnAnnotation->type === 'integer') {
-            $result = PropertyInfo\Type::BUILTIN_TYPE_INT;
-        } elseif ($columnAnnotation->type === 'string') {
-            $result = PropertyInfo\Type::BUILTIN_TYPE_STRING;
-        } else {
-            $result = null;
+        $type = RestApiBundle\Helper\TypeExtractor::extractPropertyType($reflectionProperty);
+        if (!$type) {
+            throw new \InvalidArgumentException();
         }
 
-        return $result;
+        $allowedTypes = [
+            PropertyInfo\Type::BUILTIN_TYPE_INT,
+            PropertyInfo\Type::BUILTIN_TYPE_STRING,
+        ];
+
+        if (!in_array($type->getBuiltinType(), $allowedTypes, true)) {
+            throw new \InvalidArgumentException();
+        }
+
+        return $type->getBuiltinType();
     }
 }
