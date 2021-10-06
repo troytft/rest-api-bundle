9c7d1ad1f9a28c91b7112ee1870f0eacc014bd85
Ilya Troy <troytft@gmail.com>
2021-10-06 22:50:52 +0300

    f

--- src/Services/Mapper/Transformer/EntityTransformer.php
+++ src/Services/Mapper/Transformer/EntityTransformer.php
@@ -4,6 +4,7 @@ namespace RestApiBundle\Services\Mapper\Transformer;
 
 use RestApiBundle;
 use Doctrine\ORM\EntityRepository;
+use Symfony\Component\PropertyInfo;
 use Doctrine\ORM\EntityManagerInterface;
 
 use function sprintf;
@@ -25,14 +26,14 @@ class EntityTransformer implements TransformerInterface
         $class = $options[static::CLASS_OPTION];
         $field = $options[static::FIELD_OPTION];
 
-        $fieldType = $this->entityManager->getClassMetadata($class)->getTypeOfField($field);
+        $fieldType = RestApiBundle\Helper\DoctrineHelper::extractColumnType($class, $field);
 
         if ($fieldType === null) {
             throw new \InvalidArgumentException(sprintf('Class "%s" has not a field with name "%s"', $class, $field));
-        } elseif ($fieldType === \Doctrine\DBAL\Types\Type::INTEGER) {
+        } elseif ($fieldType === PropertyInfo\Type::BUILTIN_TYPE_INT) {
             $transformer = new IntegerTransformer();
             $value = $transformer->transform($value);
-        } elseif ($fieldType === \Doctrine\DBAL\Types\Type::STRING) {
+        } elseif ($fieldType === PropertyInfo\Type::BUILTIN_TYPE_STRING) {
             $transformer = new StringTransformer();
             $value = $transformer->transform($value);
         } else {
