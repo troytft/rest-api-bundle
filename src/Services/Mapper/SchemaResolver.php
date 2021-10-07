b43684b37f7354a2e7ada6b7a67bc38f3e7056d4
Ilya Troy <troytft@gmail.com>
2021-10-07 12:01:37 +0300

    f

--- src/Services/Mapper/SchemaResolver.php
+++ src/Services/Mapper/SchemaResolver.php
@@ -7,7 +7,6 @@ use Symfony\Component\PropertyInfo;
 
 use function sprintf;
 use function ucfirst;
-use function var_dump;
 
 class SchemaResolver implements RestApiBundle\Services\Mapper\SchemaResolverInterface
 {
@@ -48,7 +47,6 @@ class SchemaResolver implements RestApiBundle\Services\Mapper\SchemaResolverInte
                     throw new RestApiBundle\Exception\Mapper\Schema\InvalidDefinitionException(sprintf('Setter with name "%s" does not exist.', $propertySetterName));
                 }
             } catch (RestApiBundle\Exception\Mapper\Schema\InvalidDefinitionException $exception) {
-                var_dump($exception->getTraceAsString());
                 throw new RestApiBundle\Exception\ContextAware\PropertyOfClassException($exception->getMessage(), class: $class, propertyName: $reflectionProperty->getName(), previous: $exception);
             }
 
