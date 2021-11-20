1472d655b332c8000dc7e94f71ff0941b0674cc0
Ilya Troy <troytft@gmail.com>
2021-11-20 14:06:39 +0300

    f

---
+++ src/Exception/ContextAware/ReflectionMethodAwareException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace RestApiBundle\Exception\ContextAware;
+
+use RestApiBundle;
+
+use function sprintf;
+
+ class ReflectionMethodAwareException extends \Exception implements RestApiBundle\Exception\ContextAware\ContextAwareExceptionInterface
+{
+    public function __construct(string $message, \ReflectionMethod $reflectionMethod)
+    {
+        parent::__construct(sprintf('%s %s->%s()', $message, $reflectionMethod->class, $reflectionMethod->name));
+    }
+}
