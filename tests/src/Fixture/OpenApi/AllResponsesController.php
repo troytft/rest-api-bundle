2f5833bf7383b5344ba2132b1caf5323d8b0bbda
Ilya Troy <troytft@gmail.com>
2021-11-23 21:25:41 +0300

    f

--- tests/src/Fixture/OpenApi/AllResponsesController.php
+++ tests/src/Fixture/OpenApi/AllResponsesController.php
@@ -4,16 +4,19 @@ namespace Tests\Fixture\OpenApi;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class AllResponsesController extends AbstractController
 {
     public function redirectResponseAction(): RedirectResponse
     {
+        return new RedirectResponse('');
     }
 
     public function binaryFileResponseAction(): BinaryFileResponse
     {
+        return new BinaryFileResponse(new File('', false));
     }
 
     public function voidResponseAction(): void
