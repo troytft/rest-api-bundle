b942ec30aa08e40ba320e888a37e7ceed4b639ca
Ilya Troy <troytft@gmail.com>
2022-01-24 01:56:34 +0300

    f

--- tests/src/Fixture/OpenApi/GenerateDocumentationCommandTest/TestSuccess/ResponseModel/Author.php
+++ tests/src/Fixture/OpenApi/GenerateDocumentationCommandTest/TestSuccess/ResponseModel/Author.php
@@ -2,13 +2,12 @@
 
 namespace Tests\Fixture\OpenApi\GenerateDocumentationCommandTest\TestSuccess\ResponseModel;
 
-use Tests;
 use RestApiBundle;
 
 class Author implements RestApiBundle\Mapping\ResponseModel\ResponseModelInterface
 {
     public function getId(): int
     {
-        return 0;
+        return 1;
     }
 }
