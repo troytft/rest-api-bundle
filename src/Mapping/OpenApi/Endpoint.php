9c53fc0b46201bd61b2d9103db2607d283b2c067
Ilya Troy <troytft@gmail.com>
2022-04-21 16:23:02 +0300

    refactoring: allow overwrite http status code for docs

--- src/Mapping/OpenApi/Endpoint.php
+++ src/Mapping/OpenApi/Endpoint.php
@@ -20,23 +20,26 @@ class Endpoint
     /** @var string[]|string */
     public $tags;
     public ?string $requestModel;
+    public ?int $httpStatusCode;
 
     /**
      * @param array|string $options
      * @param string[]|string $tags
      */
-    public function __construct($options = [], string $title = '', ?string $description = null, $tags = [], ?string $requestModel = null)
+    public function __construct($options = [], string $title = '', ?string $description = null, $tags = [], ?string $requestModel = null, ?int $httpStatusCode = null)
     {
         if (is_string($options)) {
             $this->title = $options;
             $this->description = $description;
             $this->tags = $tags;
             $this->requestModel = $requestModel;
+            $this->httpStatusCode = $httpStatusCode;
         } elseif (is_array($options)) {
             $this->title = $options['title'] ?? $options['value'] ?? $title;
             $this->description = $options['description'] ?? $description;
             $this->tags = $options['tags'] ?? $tags;
             $this->requestModel = $options['requestModel'] ?? $requestModel;
+            $this->httpStatusCode = $options['httpStatusCode'] ?? $httpStatusCode;
         } else {
             throw new \InvalidArgumentException();
         }
