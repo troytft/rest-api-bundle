bf771f6c3202a9930eabdc37d7ee56a63fb92b2e
Ilya Troy <troytft@gmail.com>
2022-07-10 20:10:29 +0300

    disable cache warmer

--- src/CacheWarmer/Mapper/SchemaCacheWarmer.php
+++ src/CacheWarmer/Mapper/SchemaCacheWarmer.php
@@ -7,20 +7,20 @@ use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerInterface;
 
 class SchemaCacheWarmer implements CacheWarmerInterface
 {
-    private RestApiBundle\Services\Mapper\CacheSchemaResolver $cacheSchemaResolver;
-    private RestApiBundle\Services\SettingsProvider $settingsProvider;
-
-    public function __construct(
-        RestApiBundle\Services\Mapper\CacheSchemaResolver $cacheSchemaResolver,
-        RestApiBundle\Services\SettingsProvider $settingsProvider
-    ) {
-        $this->cacheSchemaResolver = $cacheSchemaResolver;
-        $this->settingsProvider = $settingsProvider;
-    }
+//    private RestApiBundle\Services\Mapper\CacheSchemaResolver $cacheSchemaResolver;
+//    private RestApiBundle\Services\SettingsProvider $settingsProvider;
+//
+//    public function __construct(
+//        RestApiBundle\Services\Mapper\CacheSchemaResolver $cacheSchemaResolver,
+//        RestApiBundle\Services\SettingsProvider $settingsProvider
+//    ) {
+//        $this->cacheSchemaResolver = $cacheSchemaResolver;
+//        $this->settingsProvider = $settingsProvider;
+//    }
 
     public function warmUp($cacheDir)
     {
-        return $this->cacheSchemaResolver->warmUpCache($this->settingsProvider->getSourceCodeDirectory());
+        return [];//return $this->cacheSchemaResolver->warmUpCache($this->settingsProvider->getSourceCodeDirectory());
     }
 
     public function isOptional()
