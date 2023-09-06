bcd34cfd21482bbb97eff923be8713206addee0c
Ilya Troy <troytft@gmail.com>
2023-09-06 10:14:03 +0400

    do not use getter/setter on public properties (part3)

--- src/Services/Mapper/SchemaResolver.php
+++ src/Services/Mapper/SchemaResolver.php
@@ -59,7 +59,8 @@ class SchemaResolver implements RestApiBundle\Services\Mapper\SchemaResolverInte
                             $propertySchema->propertyGetterName = 'is' . $formattedPropertyName;
                             if (!$reflectionClass->hasMethod($propertySchema->propertyGetterName) || !$reflectionClass->getMethod($propertySchema->propertyGetterName)->isPublic()) {
                                 throw new RestApiBundle\Exception\Mapper\Schema\InvalidDefinitionException(sprintf('Property "%s" must be public or getter must exist.', $reflectionProperty->getName()));
-                            }                        }
+                            }
+                        }
                     }
                 }
             } catch (RestApiBundle\Exception\Mapper\Schema\InvalidDefinitionException $exception) {
